61f76c13db1d0ce46f162b4ef151fb761cc071a4
Tawakalt10 <raheemtawa@gmail.com>
2026-06-02 18:22:35 +0100

    new

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaDumbbell } from 'react-icons/fa'
+import { FaCircle } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -10,15 +11,17 @@ const Main = () => {
             <button className='text-gray-500  hover:text-black '>View all</button>
         </div>
         
-        <div>
-             <FaDumbbell /> 
-            <div className='flex'>
-                <h4>Lower body pressure</h4>
-                <div className='flex'>
-                 <p>Yesterday</p>
-                 <p>. 42 Mins . 320 Kcal</p>
+        <div className='flex  mx-10 lg:w-[60%] h-20 pb-4 mb-4 bg-gray-100 rounded-2xl'>
+             < FaDumbbell  className='text-2xl mx-4 my-3 '/> 
+            <div className='p-3 mx-10'>
+                <h4 className='mx-2'>Lower body pressure</h4>
+                <div className='flex pt-2  text-gray-500 text-sm'>
+                 <p className='mx-2'> Yesterday</p>
+                 <p className='mx-2' > . 42 Mins  </p>
+                 <p className='mx-2'> .  320 Kcal</p>
                 </div>
-               
+            
+        
             </div>
         </div>
         </div>
