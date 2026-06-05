9646610cc0f030ac50b57890004111503eb1c33c
Tawakalt10 <raheemtawa@gmail.com>
2026-06-05 16:28:15 +0100

    new

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -7,17 +7,18 @@ const Box = () => {
     <div>
         <div className=' mb-2 p-4 grid lg:grid-cols-4'>
             {gymInfo.map((boxData) => (
-              <div key={boxData.id} className='w-[300px] h-[150px] bg-gray-200 rounded-2xl'>
-            <div className='flex justify-between'>
-            <p><boxData.icon/></p>
-            <h4>{boxData.title}</h4>  
+              <div key={boxData.id} className='w-[300px] h-[150px]  bg-gray-200 rounded-2xl hover:scale-97 '>
+            <div className='flex justify-between mt-4'>
+            <p className='mx-6 ext-3xl mx-4     bg-gray-300  p-1.5 rounded-2xl'><boxData.icon/></p>
+            <h4 className='mx-8 text-[10px]  text-gray-500  uppercase'>{boxData.title}</h4>  
            </div>           
-           <div>
-            <h4>{boxData.value}</h4>
-            <p>{boxData.subtitle}</p>
-             <p>{boxData.progress}</p>
+           <div className='mx-6'>
+            <h4 className='font-semibold  mt-4'>{boxData.value}</h4>
+            <p className='text-[13px] text-gray-600 '>{boxData.subtitle}</p>
+             {/* <p>{boxData.progress}</p> */}
+            <p className=' my-4 w-[70%] h-1.5 bg-lime-400 rounded-2xl'></p>
            </div>
-            
+             
           </div>  
             ) )}
           
