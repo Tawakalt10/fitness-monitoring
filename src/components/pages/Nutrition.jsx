3907ad37ea77e24822a26d8ed9f76c283b9f4994
Tawakalt10 <raheemtawa@gmail.com>
2026-06-08 11:46:27 +0100

    new

--- src/components/pages/Nutrition.jsx
+++ src/components/pages/Nutrition.jsx
@@ -7,8 +7,8 @@ import foodInfo from '../data/nutritionData.js'
 const Nutrition = () => {
   return (
     <div>
-        <div className='grid lg:grid-cols-2 md:grid-cols-1'>
-  <div className='w-[80%]  ml-4 mr-4 h-60 mb-4 bg-gray-200  p-1.5 rounded-2xl'>
+        <div className='grid lg:grid-cols-2  md:grid-cols-1'>
+  <div className='lg:w-[800px]  ml-4 mr-4 h-60 mb-4 bg-gray-200  p-1.5 rounded-2xl'>
          <div className=' mt-2 mx-4 flex justify-between'>
          <h4 className=' uppercase text-sm text-gray-500 font-bold'> Nutrition today</h4>
          <p className='text-gray-500  text-sm font-bold'>1,840 / 2,400 kcal</p>
@@ -25,19 +25,22 @@ const Nutrition = () => {
            
             </div>
 
-            <div className='flex'>
-                <h4>{nutritionData.value1}</h4>
-                <h4>{nutritionData.value2}</h4>
+            <div className='flex mt-2'>
+                <h4 className=''>{nutritionData.value1}</h4>
+                <h4 className='my-2 text-[12px] text-gray-500 '>{nutritionData.value2}</h4>
             </div>
           <p className=' my-4 w-[70%] h-1.5 bg-black rounded-2xl'></p>
         </div>
 
            ))}
      </div>
-
+    
+    <div>
+        
+    </div>
    </div>
 
-   <div className=' w-[20%] h-40 bg-gray-200'>
+   <div className=' lg:w-[250px]  rounded-2xl lg:mx-70 h-[30] bg-gray-200 '>
 
    </div>
 
